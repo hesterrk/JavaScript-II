9232c37f3b50c3f56e0c6853212e34829485ee61
Hester Knight <hesterknight@hesters-mbp.mynet>
2019-12-13 12:10:12 +0000

    next challenge of cb done

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -93,24 +93,48 @@ function getLength(arr, cb) {
 getLength(items, (length) => console.log(length));
 
 
-
-
-
+//2. 
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
   return cb(arr[arr.length-1]);
 }
 
+last(items, (last) => console.log(last));
+
+//3.
+
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
-   return cb(x+y);
+   return cb(x, y);
+  
 }
+
+
+
+sumNums(6, 10, function(num1, num2) { console.log(num1 + num2) });
+
+
+
+//4. 
+
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
-  return cb(x*y);
+  // return cb(x*y);
+
+  return cb(x, y);
+
 }
+
+multiplyNums(5, 10, function(num1, num2) { console.log(num1*num2)})
+
+
+
+//5. 
+
+
+
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
